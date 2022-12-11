a67f9381b5df23278f7b44e8ff764e929a4c37d8
masuyama13 <masterxwebm13@gmail.com>
2022-12-11 20:22:18 +0900

    rubocop -A

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show update destroy ]
+  before_action :set_article, only: %i[show update destroy]
 
   # GET /articles
   def index
@@ -39,13 +41,14 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe ArticlesController, type: :routing do
@@ -9,7 +11,6 @@ RSpec.describe ArticlesController, type: :routing do
     it "routes to #show" do
       expect(get: "/articles/1").to route_to("articles#show", id: "1")
     end
-
 
     it "routes to #create" do
       expect(post: "/articles").to route_to("articles#create")

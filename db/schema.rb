f5c038928aac8e58d9fba863c661f97cf4bb48bd
masuyama13 <masterxwebm13@gmail.com>
2022-12-19 23:50:38 +0900

    rubocop -A

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_125606) do
+ActiveRecord::Schema[7.0].define(version: 20_221_219_125_606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +22,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_125606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
